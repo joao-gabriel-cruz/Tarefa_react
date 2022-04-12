f6f53df7393e31b24264282a6aa5274769cd2987
joao-gabriel-cruz <joaogabrielpinhodacruz@gmail.com>
2022-04-12 08:13:58 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState, useEffect } from 'react';
+
 
 function App() {
+
+  const [funcionarios, setFuncionarios] = useState([
+    { cpf: '431', nome: 'João', promover: false }
+
+  ])
+
+
+  function addFuncionarios(event) {
+
+
+    event.preventDefault()
+
+    const funciCpf = event.target.cpf.value
+    const funciNome = event.target.name.value
+
+    if (!funciCpf) {
+      return event.target.name.value = ''
+    }
+    if (!funciNome) {
+      return event.target.cpf.value = ''
+    }
+
+    setFuncionarios([...funcionarios, { cpf: funciCpf, name: funciNome, promover: false }])
+
+    console.log(funcionarios)
+
+    event.target.cpf.value = ''
+    event.target.name.value = ''
+
+  }
+var mostrar = false
+
+  function listFuncionarios() {
+        return mostrar = trueya
+  }
+console.log(mostrar)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+        <h1>cadastre o funcionario</h1>
+        <form onSubmit={addFuncionarios}>
+          <br />
+          <label>Nome:</label>
+          <input id="name" placeholder='Nome' />
+          <br />
+          <label>Cpf:</label>
+          <input id="cpf" placeholder='Cpf' />
+          <br />
+          <button>enviar</button>
+
+        </form>
+        <div>
+          <p>mostrar lista de funcionarios cadastrados</p>
+          <button onClick={listFuncionarios}>mostrar</button>
+        </div>
+        {mostrar ?
+             <ul>
+               {funcionarios.map(f => <li key={f.cpfcpf}>{funcionarios}</li>)}
+             </ul>: ''
+        }
+
+      </section>
     </div>
   );
 }
